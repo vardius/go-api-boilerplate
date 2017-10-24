ee474aa6744e66a354a36f3a517ee46196fedf2c
Rafał Lorenz <vardius@gmail.com>
2017-10-25 09:19:11 +1100

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,30 @@
+/*
+Package goapiboilerplate provides Go Server/API boilerplate using best practices, DDD, CQRS, ES.
+
+Directory Layout:
+  .
+  ├── /.vscode/            # Visual Studio Code remote debugging setttings
+  ├── /nginx/              # Nginx docker container configuration
+  ├── /cmd/                # Binaries
+  │   ├── /server/         # Server binary
+  │   │   └── /main.go     # Server application - glues together libraries
+  │   ├── /...             # etc.
+  ├── /pkg/                # Libraries
+  │   ├── /controller/     # Http handlers
+  │   │   ├── /dispatch.go # Dispatch command endpoint
+  │   │   ├── /...         # etc.
+  │   ├── /domain/         # Domain libraries
+  │   │   ├── /user/       # User domain
+  │   │   │   ├── /main.go # Main user domain entrypoint
+  │   │   ├── /...         # etc.
+  │   ├── /middleware/     # Http middlewares
+  │   │   ├── /cors.go     # CORS middleware
+  │   │   ├── /...         # etc.
+  ├── /vendor/             # Vendor libraries
+  ├── docker-compose.yml   # Defines Docker services, networks and volumes per developer environment
+  ├── Dockerfile           # Docker image for production
+  ├── Makefile             # Commands for building a Docker image for production and deployment
+  ├── .env                 # Enviroment configuration
+  └── bootstart.sh         # Configuration script for docker containers
+*/
+package goapiboilerplate
