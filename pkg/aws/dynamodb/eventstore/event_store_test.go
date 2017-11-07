92639d940b3877c6643c245687fe1ed639468c22
Rafał Lorenz <vardius@gmail.com>
2017-11-08 09:53:11 +1100

    Fix event store test

--- pkg/aws/dynamodb/eventstore/event_store_test.go
+++ pkg/aws/dynamodb/eventstore/event_store_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 )
 
-func TestNewEventStore(t *testing.T) {
-	bus := NewEventStore("test", &aws.Config{Region: aws.String("us-west-2")})
+func TestNew(t *testing.T) {
+	bus := New("test", &aws.Config{Region: aws.String("us-west-2")})
 
 	if bus == nil {
 		t.Fail()
