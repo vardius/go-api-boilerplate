5904fbacae9d776183db4b635b683ca6d84140df
Rafał Lorenz <vardius@gmail.com>
2018-02-15 23:46:42 +1100

    If payload is nil return empty response instead of null

--- pkg/http/response/json.go
+++ pkg/http/response/json.go
@@ -1,50 +1,57 @@
 package response
 
 import (
-	"encoding/json"
-	"net/http"
+  "encoding/json"
+  "net/http"
+  "fmt"
 )
 
 // AsJSON wraps handler and parse payload to json response
 func AsJSON(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+  fn := func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
 
-		ctx := contextWithResponse(r.Context())
+    ctx := contextWithResponse(r.Context())
 
-		next.ServeHTTP(w, r.WithContext(ctx))
+    next.ServeHTTP(w, r.WithContext(ctx))
 
-		if response, ok := fromContext(ctx); ok {
-			switch t := response.payload.(type) {
-			case HTTPError:
-				w.WriteHeader(t.Code)
-			case *HTTPError:
-				w.WriteHeader(t.Code)
-			}
+    if response, ok := fromContext(ctx); ok {
+      switch t := response.payload.(type) {
+      case HTTPError:
+        w.WriteHeader(t.Code)
+      case *HTTPError:
+        w.WriteHeader(t.Code)
+      }
 
-			encoder := json.NewEncoder(w)
-			encoder.SetEscapeHTML(true)
-			encoder.SetIndent("", "")
+      encoder := json.NewEncoder(w)
+      encoder.SetEscapeHTML(true)
+      encoder.SetIndent("", "")
 
-			err := encoder.Encode(response.payload)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				json.NewEncoder(w).Encode(HTTPError{
-					Code:    http.StatusInternalServerError,
-					Error:   err,
-					Message: http.StatusText(http.StatusInternalServerError),
-				})
-				return
-			}
+      fmt.Println(response.payload)
 
-			if f, ok := w.(http.Flusher); ok {
-				f.Flush()
-			} else {
-				// Write nil in case of setting http.StatusOK header if header not set
-				w.Write(nil)
-			}
-		}
-	}
+      if response.payload != nil {
+        err := encoder.Encode(response.payload)
 
-	return http.HandlerFunc(fn)
+        if err != nil {
+          w.WriteHeader(http.StatusInternalServerError)
+          encoder.Encode(HTTPError{
+            Code:    http.StatusInternalServerError,
+            Error:   err,
+            Message: http.StatusText(http.StatusInternalServerError),
+          })
+
+          return
+        }
+      }
+
+      if f, ok := w.(http.Flusher); ok {
+        f.Flush()
+      } else {
+        // Write nil in case of setting http.StatusOK header if header not set
+        w.Write(nil)
+      }
+    }
+  }
+
+  return http.HandlerFunc(fn)
 }
