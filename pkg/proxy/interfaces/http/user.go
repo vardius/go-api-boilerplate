48f4ff93dd49e96a5b8cb8866b9f2a40bbd2fc14
Rafał Lorenz <vardius@gmail.com>
2018-04-11 08:16:26 +1000

    Improve naming

--- pkg/proxy/interfaces/http/user.go
+++ pkg/proxy/interfaces/http/user.go
@@ -2,19 +2,19 @@ package http
 
 import (
 	"github.com/vardius/go-api-boilerplate/pkg/common/security/firewall"
-	user_http_client "github.com/vardius/go-api-boilerplate/pkg/proxy/infrastructure/user/http"
-	user_grpc_server "github.com/vardius/go-api-boilerplate/pkg/user/interfaces/grpc"
+	proxy_user_http "github.com/vardius/go-api-boilerplate/pkg/proxy/infrastructure/user/http"
+	user_grpc "github.com/vardius/go-api-boilerplate/pkg/user/interfaces/grpc"
 	user_proto "github.com/vardius/go-api-boilerplate/pkg/user/interfaces/proto"
 	"github.com/vardius/gorouter"
 )
 
 // AddUserRoutes adds user routes to router
 func AddUserRoutes(router gorouter.Router, grpClient user_proto.UserClient) {
-	httpClient := user_http_client.FromGRPC(grpClient)
+	httpClient := proxy_user_http.FromGRPC(grpClient)
 
 	subRouter := gorouter.New()
 	subRouter.POST("/dispatch/{command}", httpClient)
-	subRouter.USE(gorouter.POST, "/dispatch/"+user_grpc_server.ChangeUserEmailAddress, firewall.GrantAccessFor("USER"))
+	subRouter.USE(gorouter.POST, "/dispatch/"+user_grpc.ChangeUserEmailAddress, firewall.GrantAccessFor("USER"))
 
 	// User domain
 	router.Mount("/users", subRouter)
