829ab5f7bf24c37df16f441b49ba575e73282278
Rafał Lorenz <vardius@gmail.com>
2018-09-06 22:27:42 +1000

    Update deployments config map

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -26,16 +26,16 @@ import (
 )
 
 type config struct {
-	Env          string   `env:"ENV"             envDefault:"development"`
-	Host         string   `env:"HOST"            envDefault:"0.0.0.0"`
-	Port         int      `env:"PORT"            envDefault:"3000"`
-	UserHost     string   `env:"USER_HOST"       envDefault:"0.0.0.0"`
-	UserPort     int      `env:"USER_PORT"       envDefault:"3002"`
-	AuthHost     string   `env:"AUTH_HOST"       envDefault:"0.0.0.0"`
-	AuthPort     int      `env:"AUTH_PORT"       envDefault:"3001"`
+	Env          string   `env:"ENV"               envDefault:"development"`
+	Host         string   `env:"HOST"              envDefault:"0.0.0.0"`
+	Port         int      `env:"PORT"              envDefault:"3000"`
+	UserHost     string   `env:"USER_SERVICE_HOST" envDefault:"0.0.0.0"`
+	UserPort     int      `env:"USER_SERVICE_PORT" envDefault:"3002"`
+	AuthHost     string   `env:"AUTH_SERVICE_HOST" envDefault:"0.0.0.0"`
+	AuthPort     int      `env:"AUTH_SERVICE_PORT" envDefault:"3001"`
 	CertDirCache string   `env:"CERT_DIR_CACHE"`
-	Secret       string   `env:"SECRET"          envDefault:"secret"`
-	Origins      []string `env:"ORIGINS"         envSeparator:"|"` // Origins should follow format: scheme "://" host [ ":" port ]
+	Secret       string   `env:"SECRET"            envDefault:"secret"`
+	Origins      []string `env:"ORIGINS"           envSeparator:"|"` // Origins should follow format: scheme "://" host [ ":" port ]
 }
 
 func main() {
