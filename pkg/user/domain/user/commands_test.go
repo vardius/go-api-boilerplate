4c71786928e6a651861800044a8908be9d1cf0fc
Rafał Lorenz <vardius@gmail.com>
2019-01-08 10:44:40 +1100

    Add handler tests

--- pkg/user/domain/user/commands_test.go
+++ pkg/user/domain/user/commands_test.go
@@ -2,8 +2,11 @@ package user
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 func TestUnmarshalChangeEmailAddress(t *testing.T) {
@@ -44,5 +47,84 @@ func testUnmarshalCommand(t *testing.T, testJSON []byte, c interface{}) {
 	cmp := bytes.Compare(j, testJSON)
 	if cmp != 0 {
 		t.Errorf("Serialize command did not match expected result: %s | %d", string(j), cmp)
+	}
+}
+
+func TestOnChangeEmailAddress(t *testing.T) {
+	handler := OnChangeEmailAddress(new(repositoryMock))
+	cmd := &ChangeEmailAddress{}
+
+	testCommandHandler(t, func(ctx context.Context, out chan<- error) {
+		if f, ok := handler.(func(context.Context, *ChangeEmailAddress, chan<- error)); ok {
+			f(ctx, cmd, out)
+		} else {
+			t.Fatal("Could not call handler")
+		}
+	})
+}
+
+func TestOnRegisterWithEmail(t *testing.T) {
+	handler := OnRegisterWithEmail(new(repositoryMock))
+	cmd := &RegisterWithEmail{}
+
+	testCommandHandler(t, func(ctx context.Context, out chan<- error) {
+		if f, ok := handler.(func(context.Context, *RegisterWithEmail, chan<- error)); ok {
+			f(ctx, cmd, out)
+		} else {
+			t.Fatal("Could not call handler")
+		}
+	})
+}
+
+func TestOnRegisterWithFacebook(t *testing.T) {
+	handler := OnRegisterWithFacebook(new(repositoryMock))
+	cmd := &RegisterWithFacebook{}
+
+	testCommandHandler(t, func(ctx context.Context, out chan<- error) {
+		if f, ok := handler.(func(context.Context, *RegisterWithFacebook, chan<- error)); ok {
+			f(ctx, cmd, out)
+		} else {
+			t.Fatal("Could not call handler")
+		}
+	})
+}
+
+func TestOnRegisterWithGoogle(t *testing.T) {
+	handler := OnRegisterWithGoogle(new(repositoryMock))
+	cmd := &RegisterWithGoogle{}
+
+	testCommandHandler(t, func(ctx context.Context, out chan<- error) {
+		if f, ok := handler.(func(context.Context, *RegisterWithGoogle, chan<- error)); ok {
+			f(ctx, cmd, out)
+		} else {
+			t.Fatal("Could not call handler")
+		}
+	})
+}
+
+type repositoryMock int
+
+func (r *repositoryMock) Save(ctx context.Context, u *User) error {
+	return nil
+}
+
+func (r *repositoryMock) Get(id uuid.UUID) *User {
+	return New()
+}
+
+func testCommandHandler(t *testing.T, fn func(context.Context, chan<- error)) {
+	ctx := context.Background()
+	out := make(chan error)
+	defer close(out)
+
+	go fn(ctx, out)
+
+	select {
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	case err := <-out:
+		if err != nil {
+			t.Error(err)
+		}
 	}
 }
