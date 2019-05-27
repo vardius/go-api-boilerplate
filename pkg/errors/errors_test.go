bce1202fee9688b724e5142308e868bb7fbd6cf5
Rafał Lorenz <vardius@gmail.com>
2019-05-27 11:39:43 +1000

    Add errors tests

--- pkg/errors/errors_test.go
+++ pkg/errors/errors_test.go
@@ -40,3 +40,51 @@ func TestWrap(t *testing.T) {
 		t.Error("Error message does not match")
 	}
 }
+
+func TestErrorMessage(t *testing.T) {
+	msg := "internal error"
+	err := New(INTERNAL, msg)
+
+	if ErrorMessage(nil) != "" {
+		t.Error("Error message does not match")
+	}
+
+	if ErrorMessage(err) != msg {
+		t.Error("Error message does not match")
+	}
+}
+
+func TestErrorCode(t *testing.T) {
+	msg := "internal error"
+	err := New(INTERNAL, msg)
+
+	if ErrorCode(nil) != "" {
+		t.Error("Error message does not match")
+	}
+
+	if ErrorCode(err) != INTERNAL {
+		t.Error("Error message does not match")
+	}
+}
+
+func TestError(t *testing.T) {
+	if New("", "").Error() != "" {
+		t.Error("Error string representation of the error message is invalid")
+	}
+
+	if New(INTERNAL, "").Error() == "" {
+		t.Error("Error string representation of the error message is invalid")
+	}
+
+	if New("", "internal error").Error() == "" {
+		t.Error("Error string representation of the error message is invalid")
+	}
+
+	if New(INTERNAL, "internal error").Error() == "" {
+		t.Error("Error string representation of the error message is invalid")
+	}
+
+	if Wrap(New(INTERNAL, "internal error"), "", "").Error() == "" {
+		t.Error("Error string representation of the error message is invalid")
+	}
+}
--- pkg/errors/http_test.go
+++ pkg/errors/http_test.go
@@ -1,14 +1,37 @@
 package errors
 
 import (
+	nativeErrors "errors"
 	"net/http"
 	"testing"
 )
 
 func TestHTTPStatusCode(t *testing.T) {
-	err := New("internal error", INTERNAL)
+	if HTTPStatusCode(nil) != http.StatusOK {
+		t.Error("Status code does not match")
+	}
 
-	if HTTPStatusCode(err) != http.StatusInternalServerError {
+	if HTTPStatusCode(New(INTERNAL, "internal error")) != http.StatusInternalServerError {
+		t.Error("Status code does not match")
+	}
+
+	if HTTPStatusCode(New(FORBIDDEN, "status map")) != errorCodeToHTTPStatusMap[FORBIDDEN] {
+		t.Error("Status code does not match")
+	}
+
+	if HTTPStatusCode(Wrap(New(INTERNAL, "status map"), "", "")) != errorCodeToHTTPStatusMap[INTERNAL] {
+		t.Error("Status code does not match")
+	}
+
+	if HTTPStatusCode(New("unknown_code", "internal error")) != http.StatusInternalServerError {
+		t.Error("Status code does not match")
+	}
+
+	if HTTPStatusCode(New("123", "code as int")) != 123 {
+		t.Error("Status code does not match")
+	}
+
+	if HTTPStatusCode(nativeErrors.New("native error")) != http.StatusInternalServerError {
 		t.Error("Status code does not match")
 	}
 }
