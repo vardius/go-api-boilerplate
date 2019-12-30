89ec4254a33c916c27095f94f84739ff5f51e21e
mar1n3r0 <m.kirkov@gmail.com>
2019-12-30 20:37:08 +0200

    Add github.com/vardius/gollback.Gollback).Retry errcheck

--- cmd/auth/internal/application/eventhandler/register.go
+++ cmd/auth/internal/application/eventhandler/register.go
@@ -26,10 +26,14 @@ func Register(conn *grpc.ClientConn, eventBus eventbus.EventBus, topicToHandlerM
 			for topic, handler := range topicToHandlerMap {
 				// Will resubscribe to handler on error infinitely
 				go func(topic string, handler eventbus.EventHandler) {
-					gollback.New(context.Background()).Retry(0, func(ctx context.Context) (interface{}, error) {
+					_, err := gollback.New(context.Background()).Retry(0, func(ctx context.Context) (interface{}, error) {
 						err := eventBus.Subscribe(ctx, topic, handler)
 						return nil, errors.Newf(errors.INTERNAL, "EventHandler %s unsubscribed (%v)", topic, err)
 					})
+
+					if err != nil {
+						panic(err)
+					}
 				}(topic, handler)
 			}
 
