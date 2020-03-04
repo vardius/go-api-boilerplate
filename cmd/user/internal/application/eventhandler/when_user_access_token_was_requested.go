0e2677a10569aa39b6dad952cb4d88e82a2b2adc
mar1n3r0 <m.kirkov@gmail.com>
2020-03-04 23:46:21 +0200

    fix hound warnings

--- cmd/user/internal/application/eventhandler/when_user_access_token_was_requested.go
+++ cmd/user/internal/application/eventhandler/when_user_access_token_was_requested.go
@@ -14,7 +14,7 @@ import (
 	"github.com/vardius/go-api-boilerplate/internal/eventbus"
 )
 
-// Create a struct that will be encoded to a JWT.
+// Claims Create a struct that will be encoded to a JWT.
 // We add jwt.StandardClaims as an embedded type, to provide fields like expiry time
 type Claims struct {
 	Username string `json:"username"`
