ee5207f8411354a85acf125ec26e94f87ee9c48d
Rafał Lorenz <rafal@autopilothq.com>
2020-03-23 09:20:05 +1100

    Fix invalid payload test

--- pkg/http/response/json_test.go
+++ pkg/http/response/json_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/vardius/go-api-boilerplate/pkg/errors"
 )
 
-func TestRespondJSON(t *testing.T) {
+func TestJSON(t *testing.T) {
 	type jsonResponse struct {
 		Name string `json:"name"`
 	}
@@ -32,16 +32,16 @@ func TestRespondJSON(t *testing.T) {
 
 	header := w.Header()
 	if header.Get("Content-Type") != "application/json" {
-		t.Error("RespondJSON did not set proper headers")
+		t.Error("JSON did not set proper headers")
 	}
 
 	cmp := bytes.Compare(w.Body.Bytes(), append([]byte(`{"name":"John"}`), 10))
 	if cmp != 0 {
-		t.Errorf("RespondJSON returned wrong body: %s | %d", w.Body.String(), cmp)
+		t.Errorf("JSON returned wrong body: %s | %d", w.Body.String(), cmp)
 	}
 }
 
-func TestRespondJSONNil(t *testing.T) {
+func TestJSONNil(t *testing.T) {
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if err := JSON(r.Context(), w, nil); err != nil {
 			t.Fatal(err)
@@ -58,15 +58,15 @@ func TestRespondJSONNil(t *testing.T) {
 
 	header := w.Header()
 	if header.Get("Content-Type") != "application/json" {
-		t.Error("RespondJSON did not set proper headers")
+		t.Error("JSON did not set proper headers")
 	}
 
 	if w.Code != http.StatusNoContent {
-		t.Errorf("RespondJSON error code does not match %d", w.Code)
+		t.Errorf("JSON error code does not match %d", w.Code)
 	}
 }
 
-func TestRespondJSONNilWithStatusOk(t *testing.T) {
+func TestJSONNilWithStatusOk(t *testing.T) {
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		WriteHeader(r.Context(), w, http.StatusOK)
 
@@ -85,15 +85,15 @@ func TestRespondJSONNilWithStatusOk(t *testing.T) {
 
 	header := w.Header()
 	if header.Get("Content-Type") != "application/json" {
-		t.Error("RespondJSON did not set proper headers")
+		t.Error("JSON did not set proper headers")
 	}
 
 	if w.Code != http.StatusOK {
-		t.Errorf("RespondJSON error code does not match %d", w.Code)
+		t.Errorf("JSON error code does not match %d", w.Code)
 	}
 }
 
-func TestRespondJSONError(t *testing.T) {
+func TestJSONError(t *testing.T) {
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		appErr := errors.New(errors.INVALID, "Invalid request")
 
@@ -114,27 +114,18 @@ func TestRespondJSONError(t *testing.T) {
 
 	header := w.Header()
 	if header.Get("Content-Type") != "application/json" {
-		t.Error("RespondJSONError did not set proper headers")
+		t.Error("JSON did not set proper headers")
 	}
 
 	if w.Code != http.StatusBadRequest {
-		t.Errorf("RespondJSONError error code not handled %d", w.Code)
+		t.Errorf("JSON error code not handled %d", w.Code)
 	}
 }
 
 func TestInvalidPayloadAsJSON(t *testing.T) {
-	paniced := false
-	defer func() {
-		if rcv := recover(); rcv != nil {
-			paniced = true
-		}
-	}()
-
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		WriteHeader(r.Context(), w, http.StatusOK)
-
 		if err := JSON(r.Context(), w, make(chan int)); err != nil {
-			t.Fatal(err)
+			WriteHeader(r.Context(), w, http.StatusInternalServerError)
 		}
 	})
 
@@ -148,14 +139,10 @@ func TestInvalidPayloadAsJSON(t *testing.T) {
 
 	header := w.Header()
 	if header.Get("Content-Type") != "application/json" {
-		t.Error("RespondJSON did not set proper headers")
-	}
-
-	if paniced == true {
-		t.Error("Did not panic")
+		t.Error("JSON did not set proper headers")
 	}
 
 	if w.Code != http.StatusInternalServerError {
-		t.Errorf("RespondJSON error code not handled %d", w.Code)
+		t.Errorf("JSON error code not handled %d", w.Code)
 	}
 }
