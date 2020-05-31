9afdd1bbad1d8427063ca498f1bafbc21fdb60be
Rafał Lorenz <vardius@gmail.com>
2020-06-01 09:33:10 +1000

    Update example_test.go

--- pkg/errors/example_test.go
+++ pkg/errors/example_test.go
@@ -23,7 +23,7 @@ func ExampleWrap() {
 	fmt.Printf("%s\n", err)
 
 	// Output:
-	// /home/travis/gopath/src/github.com/vardius/go-api-boilerplate/pkg/errors/example_test.go:20
+	// /home/travis/gopath/src/github.com/vardius/go-api-boilerplate/pkg/errors/example_test.go:21
 	// example:
-	// 	/home/travis/gopath/src/github.com/vardius/go-api-boilerplate/pkg/errors/example_test.go:19
+	// 	/home/travis/gopath/src/github.com/vardius/go-api-boilerplate/pkg/errors/example_test.go:20
 }
