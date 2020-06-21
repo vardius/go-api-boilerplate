06e5d25aecaf465580bf5828c33db8eb14c55d11
Rafał Lorenz <vardius@gmail.com>
2020-06-21 17:31:11 +1000

    Fix os exit

--- pkg/application/app.go
+++ pkg/application/app.go
@@ -61,7 +61,9 @@ func (app *App) Run(ctx context.Context) {
 		for i := 0; i < len(app.adapters); i++ {
 			if err := <-errCh; err != nil {
 				app.logger.Critical(ctxWithTimeout, "shutdown error: %v\n", err)
-				os.Exit(1)
+				// calling Goexit terminates that goroutine without returning (previous defers would not run)
+				defer os.Exit(1)
+				return
 			}
 		}
 
