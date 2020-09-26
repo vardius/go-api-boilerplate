972ef578a9d90231d2696c5bd6546e5f8d32fa59
Rafał Lorenz <vardius@gmail.com>
2020-09-26 20:19:13 +1000

    Update script to generate ts test workflows

--- .github/workflows.sh
+++ .github/workflows.sh
@@ -1,7 +1,8 @@
 # read the workflow templates
-TEMPLATE_TEST=$(cat ./workflow-templates/test.yaml)
-TEMPLATE_RELEASE=$(cat ./workflow-templates/release.yaml)
 TEMPLATE_PUBLISH=$(cat ./workflow-templates/publish.yaml)
+TEMPLATE_RELEASE_GO=$(cat ./workflow-templates/release-go.yaml)
+TEMPLATE_TEST_GO=$(cat ./workflow-templates/test-go.yaml)
+TEMPLATE_TEST_TS=$(cat ./workflow-templates/test-ts.yaml)
 
 # iterate each service in cmd directory
 for CMD in $(ls ../cmd); do
@@ -12,10 +13,16 @@ for CMD in $(ls ../cmd); do
 
     if [ -e ../cmd/${CMD}/main.go ] # generate this workflows only if go service
     then
-#      RELEASE=$(echo "${TEMPLATE_RELEASE}" | sed "s/{{CMD}}/${CMD}/g")
+#      RELEASE=$(echo "${TEMPLATE_RELEASE_GO}" | sed "s/{{CMD}}/${CMD}/g")
 #      echo "${RELEASE}" > ./workflows/${CMD}-release.yaml
 
-      TEST=$(echo "${TEMPLATE_TEST}" | sed "s/{{CMD}}/${CMD}/g")
+      TEST=$(echo "${TEMPLATE_TEST_GO}" | sed "s/{{CMD}}/${CMD}/g")
+      echo "${TEST}" > ./workflows/${CMD}-test.yaml
+    fi
+
+    if [ -e ../cmd/${CMD}/package.json ]
+    then
+      TEST=$(echo "${TEMPLATE_TEST_TS}" | sed "s/{{CMD}}/${CMD}/g")
       echo "${TEST}" > ./workflows/${CMD}-test.yaml
     fi
 done
