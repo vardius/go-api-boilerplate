56828c4fb10be1cea842463709f19fc94397d25f
Rafał Lorenz <vardius@gmail.com>
2021-03-03 23:34:13 +1100

    Update JSONRawMessage

--- pkg/mongo/json.go
+++ pkg/mongo/json.go
@@ -2,32 +2,29 @@ package mongo
 
 import (
 	"encoding/json"
+	"fmt"
 	apperrors "github.com/vardius/go-api-boilerplate/pkg/errors"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
 )
 
 type JSONRawMessage json.RawMessage
 
-func (m JSONRawMessage) MarshalBSON() ([]byte, error) {
-	v, err := bson.Marshal(string(m))
-	if err != nil {
-		return nil, apperrors.Wrap(err)
-	}
-	return v, nil
+func (m JSONRawMessage) MarshalBSONValue() (bsontype.Type, []byte, error) {
+	return bsontype.String, bsoncore.AppendString(nil, string(m)), nil
 }
 
-func (m *JSONRawMessage) UnmarshalBSON(data []byte) error {
+func (m *JSONRawMessage) UnmarshalBSONValue(t bsontype.Type, data []byte) error {
 	if len(data) == 0 {
 		return nil
 	}
-
-	var v string
-	if err := bson.Unmarshal(data, &v); err != nil {
-		return apperrors.Wrap(err)
+	if t != bsontype.String {
+		return apperrors.Wrap(fmt.Errorf("invalid type: %s", t))
 	}
 
-	if v != "" {
-		*m = JSONRawMessage(v)
+	str, _, ok := bsoncore.ReadString(data)
+	if ok {
+		*m = JSONRawMessage(str)
 	}
 
 	return nil
